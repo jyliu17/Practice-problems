ee9a77e1195e43e592d9181e72cccf4f0bad26b0
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-06-11 01:06:47 -0400

    Update threeNumberSum.js

--- threeNumberSum.js
+++ threeNumberSum.js
@@ -25,3 +25,33 @@ function threeSum(nums)
         while(nums[i+1] === nums[i]) i++;
     }
     return result;    
+
+
+function threeNumberSum(array, targetSum) {
+  // Write your code here.
+	array.sort((a,b) => a-b);
+	
+	const result = [];
+	for (let i = 0; i < array.length - 2; i++) {
+        let left = i + 1;
+				let right = array.length - 1;
+        
+        while(left < right) {
+            const currentSum = array[i] + array[left] + array[right];
+            
+            if(currentSum === targetSum) {
+                result.push([array[i], array[left], array[right]]);
+                left++;
+                right--;
+            } else if(currentSum < targetSum) {
+							left++;
+						} else if(currentSum > targetSum) {
+							right--;
+						}
+        }
+    }
+    return result;    
+}
+
+// Do not edit the line below.
+exports.threeNumberSum = threeNumberSum;
