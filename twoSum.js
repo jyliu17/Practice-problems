1bfd808a50f8eb15ffd44f6243ec410030de3a49
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-06-13 16:52:33 -0400

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -7,7 +7,7 @@ function sumByBruteForce(array, sum) {
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (array[i] + array[j] === sum) {
-            return true;
+            return [array[i] , array[j]];
             }
         }
     }
