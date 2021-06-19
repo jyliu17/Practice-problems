62b2db84028610ba977becae7256cf808b1e24bc
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-06-19 19:06:33 -0400

    Update nonConstructableChange.js

--- nonConstructableChange.js
+++ nonConstructableChange.js
@@ -1,2 +1,14 @@
 //Given an array of positive integers (representing coins), find the smallest value that cannot be constructed from those integers.
 
+function nonConstructibleChange(coins) {
+  // Write your code here.
+	coins.sort((a,b) => a-b);
+	
+	let currentChange = 0
+	for (const coin of coins) {
+		if (coin > currentChange +1) return currentChange + 1
+		
+		currentChange += coin
+	}
+  return currentChange + 1;
+}
