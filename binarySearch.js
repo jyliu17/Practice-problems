f007b6db9360e64638281595a5c38a6141952375
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-06-19 20:05:58 -0400

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,1 +1,17 @@
 
+function binarySearch(array, target) {
+  // Write your code here.
+	let left = 0
+	let right = array.length - 1;
+	
+	while (left <= right) {
+		let m = Math.floor((left + right)/2)
+		if (target === array[m]) {
+			return m
+		} else if (target < array[m]) {
+			right = m - 1
+		} else {
+			left = m + 1
+		}
+	} return -1
+}
