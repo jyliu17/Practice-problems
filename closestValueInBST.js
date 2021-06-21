d6a1774be02fceaf30bd542e6417e2141f3bea33
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-06-20 22:46:37 -0400

    Update and rename closestValueInBST to closestValueInBST.js

---
+++ closestValueInBST.js
@@ -0,0 +1,4 @@
+
+//Write a function that takes in a Binary Search Tree (BST) and a target integer value and returns the closest value to that target value contained in the BST.
+
+// You can assume that there will only be one closest value.
