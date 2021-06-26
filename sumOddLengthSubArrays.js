204a85f6658a3e555af81b85e299a26fed99ab5c
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-06-25 23:26:18 -0400

    Update sumOddLengthSubArrays.js

--- sumOddLengthSubArrays.js
+++ sumOddLengthSubArrays.js
@@ -6,3 +6,18 @@ A subarray is a contiguous subsequence of the array.
 
 Return the sum of all odd-length subarrays of arr.
  */
+
+//O(n^2)
+
+var sumOddLengthSubarrays = function (arr) {
+    let sum = 0;
+    for (let i = 0; i <= arr.length - 1; i++) {
+        for (let j = arr.length - 1; j >= i; j--) {
+            let tmp = (arr.slice(i, j + 1));
+            if (tmp.length % 2 != 0) {
+                sum += tmp.reduce((a, b) => a + b, 0)
+            }
+        }
+    }
+    return sum;
+};
