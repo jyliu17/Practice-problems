800c0a8910b8b89a620e2659c7ccd15b05c0bb46
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-07-18 23:25:50 -0400

    Update binarySearchTree.js

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -4,3 +4,5 @@ In this excercise we will be creating a class for a Binary Search Tree.  This cl
 1. Insert values iwth the insert method.
 2. Search values with the contains method.
 3. Remove values with the remove method; this method will only remove the first instance of a given value.
+
+**/
