685cb5cb796721c26a197af4f26dba0654c09166
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-07-25 21:15:30 -0400

    Update reverseInteger.js

--- reverseInteger.js
+++ reverseInteger.js
@@ -63,3 +63,35 @@ INTMAX
 Similar logic can be applied when \text{rev}rev is negative.
 
 **/
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    const absoluteNumber = Math.abs(x);
+    const threshold = Math.pow(2, 31);
+    
+    if (x === 0) { 
+        return 0 
+    }
+    
+    const isSigned = x > 0 ? false : true;
+    // reverse the string representation of the number 
+    // and remove prepending zeros
+    const cleanedNumber = `${absoluteNumber}`
+        .split('')
+        .reverse()
+        .filter(d => d != 0)
+        .join(''); 
+    
+	// return zero for the output that's bigger than 32 bit
+    if (
+        cleanedNumber > threshold - 1 || 
+        cleanedNumber < -threshold
+    ) {
+        return 0
+    }
+    
+    return isSigned ? -cleanedNumber : cleanedNumber;
+};
