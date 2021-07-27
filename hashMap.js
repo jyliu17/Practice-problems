1d52e2eba07c40f416e1da5dab29672f57ff4233
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-07-26 23:27:12 -0400

    Update hashMap.js

--- hashMap.js
+++ hashMap.js
@@ -14,3 +14,15 @@ engineers.set('Greg', new Engineer("Greg", "Javascript", "010-101-0011"))
 engineers.set('Jesse', new Engineer("Jesse", "Swift", "101-010-1100"))
 engineers.set('Viral', new Engineer("Viral", "Python", "111-000-1110"))
 engineers.set('Johnson', new Engineer("Johnson", "Javascript", "000-111-0001"))
+
+
+document.write("<br> Each engineer's favorite language")
+engineers.forEach(engineer => {
+    document.write('<br><br>' + engineer.name + "'s favorite language is " + engineer.favLanguage + 
+                    " and is reachable at " + engineer.number)
+})
+
+
+document.write("<br><br>" + engineers.get('Johnson').number )
+
+document.write("<br><br>" + engineers.size)
