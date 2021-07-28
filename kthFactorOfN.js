333571af66e45bb171a49cd4a83b7c15d4e4103f
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-07-27 23:46:41 -0400

    Update kthFactorOfN.js

--- kthFactorOfN.js
+++ kthFactorOfN.js
@@ -1,1 +1,11 @@
 
+/*
+
+Given two positive integers n and k.
+
+A factor of an integer n is defined as an integer i where n % i == 0.
+
+Consider a list of all factors of n sorted in ascending order, return the kth factor in this list or return -1 if n has less than k factors.
+
+
+*/
