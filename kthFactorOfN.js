ac25f914b62c340186e0cbd059ca7dc51740b5ec
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-07-28 23:21:44 -0400

    Update kthFactorOfN.js

--- kthFactorOfN.js
+++ kthFactorOfN.js
@@ -37,3 +37,15 @@ Output: 4
 Explanation: Factors list is [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 1000].
 
 */
+
+var kthFactor = function(n,k) {
+  
+  let count = 0;
+  for (let i=1; i <n; i++){
+    
+    if (n % i == 0) count++;
+    if (count == k) return i;
+  
+  }
+  return -1
+}
