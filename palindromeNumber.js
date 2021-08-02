cc8c4cc8056d534f2c7cb5fc3ff586db4babcf0e
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-08-01 23:30:41 -0400

    Update palindromeNumber.js

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -23,3 +23,31 @@ Example 4:
 Input: x = -101
 Output: false
 **/
+
+/** two pointers **/
+
+var isPalindrome = function(x) {
+    
+    if (x < 0) {
+        return false;
+    }
+    
+    if ( x % 10 === 0 && x !== 0) {
+        return false
+    }
+    
+    
+    const str = String(x)
+    let left = 0
+    let right = str.length - 1
+    
+    while (left < right) {
+        if (str[left] !== str[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    
+    return true;
+};
