3bdc4f22954e1a60cc77eff5bf7b089375206d43
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-08-15 23:53:22 -0400

    Update validParantheses.js

--- validParantheses.js
+++ validParantheses.js
@@ -26,3 +26,25 @@ Output: false
 
 **/
 
+var isValid = function(s) {
+    
+ if (s.length ==0 || s.length%2 != 0){
+        return false;
+    }  
+    
+ const stack = [];
+  const closingParen = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      stack.push(s[i]);    
+    } else if (closingParen[stack.pop()] !== s[i]) {
+      return false;
+    }
+  }
+  return stack.length ? 0 : 1;
+};
+
