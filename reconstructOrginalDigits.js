b022fe125c66687b0ccd429b762cffd0006dba90
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-08-21 23:59:31 -0400

    Update reconstructOrginalDigits.js

--- reconstructOrginalDigits.js
+++ reconstructOrginalDigits.js
@@ -1,1 +1,14 @@
+/** Given a string s containing an out-of-order English representation of digits 0-9, return the digits in ascending order.
 
+ 
+
+Example 1:
+
+Input: s = "owoztneoer"
+Output: "012"
+Example 2:
+
+Input: s = "fviefuro"
+Output: "45"
+
+**/
