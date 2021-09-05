60844ac8985d386057a21f1e3c282df210fc9a49
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-09-05 17:52:51 -0400

    Update mergeTwoSortedList.js

--- mergeTwoSortedList.js
+++ mergeTwoSortedList.js
@@ -19,3 +19,21 @@ Input: l1 = [], l2 = [0]
 Output: [0]
 
 **/
+
+
+var mergeTwoLists = function(l1, l2) {
+    const result = {val : 0, next : null};
+    let cur = result;
+    while(l1 && l2){
+        if(l1.val < l2.val){
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+    cur.next = l1 || l2;
+    return result.next;
+};
