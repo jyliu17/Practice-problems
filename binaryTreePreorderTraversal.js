770c0f73ba3d4600f83632e317fea516c8437328
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-09-12 22:32:56 -0400

    Update binaryTreePreorderTraversal.js

--- binaryTreePreorderTraversal.js
+++ binaryTreePreorderTraversal.js
@@ -25,3 +25,34 @@ Example 5:
 Input: root = [1,null,2]
 Output: [1,2]
 **/
+
+/** Iterative:
+The approach for an iterative solution:
+Iterative is more complicated than recursive. The key is to use a stack to store nodes whose values haven’t been pushed yet. So for pre-order traversal, we only need to store right values on the stack, because left values can be immediately pushed to result.
+
+Result, Stack = [ ]
+Node = root
+While node is not null OR stack is not empty (this lets our loop end when we remove the final node from the stack, which would be our last ‘right-hand’ node)
+If node is null, pop a node from the stack and assign that to node (aka, get right sibling)
+Result.push node.value
+Node = node.left
+
+**/ 
+var preorderTraversal = function(root) {
+    const result = [];
+  
+    const stack = [];
+    let node = root;
+    while (node !== null || stack.length !== 0) {
+      if (node === null) {
+        node = stack.pop(); 
+      }
+      result.push(node.val);
+      if (node.right) {
+        stack.push(node.right);
+      }
+      node = node.left;
+    }
+    
+    return result;
+};
