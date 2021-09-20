c344e63476b07355aa111f81601ba05992079a03
jyliu17 <66791392+jyliu17@users.noreply.github.com>
2021-09-19 22:12:10 -0400

    Update depthFirstSearch.js

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -1,1 +1,10 @@
 
+/*** Depth-first Search. (DFS) 
+
+You are given a Node class that has a name and an array of optional children nodes. When put together, nodes form an acyclic tree like structure. 
+
+Implement the depthFirstSearch method on the Node class, which takes in an empty array, traverses the tree using the Depth-first Search approach
+
+(specifically navigating the tree from left to right), stores all of the nodes names in the put array, and returns it.
+
+**/
